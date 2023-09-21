795f723309dd089c8c9f467eda33607ee1683b0f
Laboratorio 109 Pc-13 <lab-109-inf-13>
2023-09-21 19:49:12 -0400

    Agregados nuevos metodos

--- src/Simpleinventario.java
+++ src/Simpleinventario.java
@@ -2,12 +2,24 @@ import java.util.Scanner;
 
 public class Simpleinventario {
 
+    public static void agregarArticulo(String s) {
+        System.out.println("agregar un articulo");
+
+    }
+
+    public static void RemoverArticulo(int id) {
+    }
+
+    public static double obtenerPrecioArticulo(int in) {
+        return 0.00;
+    }
 
 
+    public static void imprimirinventario() {
+        System.out.println("ingrese una opcion;");
+    }
 
-
-
-
-
-
+    public static void main(String[] args) {
+        imprimirinventario();
+    }
 }
