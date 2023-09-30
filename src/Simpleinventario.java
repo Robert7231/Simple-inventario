1d340659b05d0dd0f58efc3edc375273b7166f17
Robert7231 <robertencarnacionmontero@gmail.com>
2023-09-30 12:27:30 -0400

    Nueva clase agragada

--- src/Simpleinventario.java
+++ src/Simpleinventario.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
 
 public class Simpleinventario {
-
-    public static void agregarArticulo(String s) {
-        System.out.println("agregar un articulo");
-
+    public Simpleinventario() {
     }
 
-    public static void RemoverArticulo(int id) {
+    public static void agragarinventario(String s) {
+        System.out.println("Agregar un Articulo: ");
     }
 
-    public static double obtenerPrecioArticulo(int in) {
-        return 0.00;
+    public static String obtenerNombreArticulo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el nombre de articulo: ");
+        String nombredeArticulo = scanner.next();
+        System.out.println("Usted a ingresado el articulo" + nombredeArticulo);
+        return nombredeArticulo;
     }
 
+    public static void removerArticuloint(int id) {
+    }
+
+    public static double ObtenerPrecioArticulo(int id) {
+        return 0.0;
+    }
 
     public static void imprimirinventario() {
-        System.out.println("ingrese una opcion;");
+        System.out.println("Ingrese su Nombre");
+        Scanner scanner = new Scanner(System.in);
+        String usuario = scanner.next();
+        System.out.println("Bienvenido " + usuario);
+        String articuloExistente = obtenerNombreArticulo();
+        System.out.println("Articulo existente: " + articuloExistente);
     }
 
     public static void main(String[] args) {
