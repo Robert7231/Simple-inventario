4a125910b8ddb23834065229f705ccbb03715b59
Robert7231 <robertencarnacionmontero@gmail.com>
2023-10-22 21:28:33 -0400

    Nuevo metodo para modificar articulo

--- src/SimpleInventario.java
+++ src/SimpleInventario.java
@@ -1,56 +1,46 @@
-import java.security.PublicKey;
 import java.util.Scanner;
+
 public class SimpleInventario {
-    public static void agregarinventario(String s) {
-        System.out.println("Agregar un Articulo: ");
-        if (s.length() > 4) {
-            System.out.printf("Articulo agregado Exitosamente");
+    public static double obtenerPrecioArticulo() {
+        double random = Math.random() * 5000 + 100;
+        return random > 0 ? random : -1;
+    }
+
+    public static void modificarArticulo() {
+        String nombre = obtenerNombreArticulo();
+        double precio = obtenerPrecioArticulo();
+
+        if (nombre.matches("[adAD].*")) {
+            precio *= (precio >= 150 && precio <= 250) ? 1.02 : (precio > 250 && precio <= 500) ? 1.08 : 1.12;
+        } else if (nombre.matches("[cmCM].*")) {
+            precio *= 0.80;
         } else {
-            System.out.println("No se permite el Articulo");
+            System.out.println("El artículo no cumple con los criterios para modificar el precio.");
+            return;
         }
 
+        System.out.printf("Artículo %s tiene un precio actualizado de RD$%.2f%n", nombre, precio);
+    }
+
+    public static void agregarInventario(String s) {
+        System.out.println("Agregar un Articulo: " + (s.length() > 4 ? "Articulo agregado Exitosamente" : "No se permite el Articulo"));
     }
 
     public static String obtenerNombreArticulo() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese el nombre de articulo: ");
-        String nombredeArticulo;
-        nombredeArticulo = scanner.next();
-        System.out.println("Usted a ingresado el articulo" + nombredeArticulo);
-        return nombredeArticulo;
-
+        System.out.println("Ingrese el nombre de artículo: ");
+        return scanner.next();
     }
 
-
-    public static void removerArticuloint(int id) {
-        if (id > 0.00) {
-            System.out.println("Articulo Removido");
-        }
-    }
-
-
-    public static String ObtenerPrecioArticulo(String articulo, double precio) {
-        double random = (Math.random() * 5000) + 100;
-        if (articulo.startsWith("mar") && precio > 0.00) {
-            return "Articulo " + articulo + " tiene un valor de RD$ " + random;
-        } else if (articulo.startsWith("tab") || precio >= 12000.00) {
-            return "Articulo " + articulo + " tiene un valor que sobrepasa 12,000.00. " + " El monto de RD$ " + random;
-        }else {
-            return "Ninguno de los escenarios pudo aplicar";
-        }
-    }
-
-    public static void imprimirinventario () {
+    public static void imprimirInventario() {
         System.out.println("Ingrese su Nombre");
-        Scanner scanner = new Scanner(System.in);
-        String usuario;
-        usuario = scanner.next();
+        String usuario = new Scanner(System.in).next();
         System.out.println("Bienvenido " + usuario);
         String articuloExistente = obtenerNombreArticulo();
-        System.out.println("Articulo existente: " + articuloExistente);
+        System.out.println("Artículo existente: " + articuloExistente);
     }
 
-    public static void main (String[]args){
-        imprimirinventario();
+    public static void main(String[] args) {
+        modificarArticulo();
     }
 }
